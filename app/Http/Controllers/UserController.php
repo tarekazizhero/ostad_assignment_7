fb9da3cb20f60154a0dfcf96c23fca0d96754e45
tarek aziz <1720114.tarekaziz@gmail.com>
2023-11-10 12:32:04 +0600

    first laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function ostad(){
+        return view('welcome');
+}
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+ 
+Route::get('/', [UserController::class, 'ostad']);
+
+
+
+
